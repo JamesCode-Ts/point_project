58ed4fb96ede987367f63d5505ae34a90733414a
Tiago <jameslob42@gmail.com>
2024-01-24 16:10:19 -0300

    contrução de tabelas

--- src/main/java/servlet/UsuarioServlet.java
+++ src/main/java/servlet/UsuarioServlet.java
@@ -17,6 +17,7 @@ public class UsuarioServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+    	
         // Obter parâmetros do formulário
         String entrada = request.getParameter("nome");
         String saida = request.getParameter("email");
@@ -25,7 +26,7 @@ public class UsuarioServlet extends HttpServlet {
          System.out.println(entrada);
 
         // Construir a resposta para a requisição Ajax
-        String resposta = "Entradas e Saídas recebidas:\n Entrada: " + entrada + "\nEmail: " + saida;
+        String resposta = "Entradas e Saídas recebidas:\n Entrada: " + entrada + "\nSaida: " + saida;
 
         // Enviar a resposta de volta para a página HTML
         response.setContentType("text/plain");
